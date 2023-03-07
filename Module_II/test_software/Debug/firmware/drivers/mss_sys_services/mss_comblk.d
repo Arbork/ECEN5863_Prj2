708fad39926a9f040bd5ed8de9ab23ac99f1ba52
Alexander Bork <albo7471@colorado.edu>
2023-03-07 12:12:53 -0700

    test_software, the firmware project in softconsole is being added

---
+++ Module_II/test_software/Debug/firmware/drivers/mss_sys_services/mss_comblk.d
@@ -0,0 +1,26 @@
+firmware/drivers/mss_sys_services/mss_comblk.o: \
+ ../firmware/drivers/mss_sys_services/mss_comblk.c \
+ ../firmware/drivers/mss_sys_services/mss_comblk.h \
+ ../firmware/drivers/mss_sys_services/../../CMSIS/m2sxxx.h \
+ C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cm3.h \
+ C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cmInstr.h \
+ C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/cmsis_gcc.h \
+ C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cmFunc.h \
+ ../firmware/drivers/mss_sys_services/mss_comblk_page_handler.h \
+ ../firmware/drivers/mss_sys_services/../../CMSIS/mss_assert.h
+
+../firmware/drivers/mss_sys_services/mss_comblk.h:
+
+../firmware/drivers/mss_sys_services/../../CMSIS/m2sxxx.h:
+
+C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cm3.h:
+
+C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cmInstr.h:
+
+C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/cmsis_gcc.h:
+
+C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cmFunc.h:
+
+../firmware/drivers/mss_sys_services/mss_comblk_page_handler.h:
+
+../firmware/drivers/mss_sys_services/../../CMSIS/mss_assert.h:
---
+++ Module_II/test_software/Debug/main.d
@@ -0,0 +1,21 @@
+main.o: ../main.c ../firmware/drivers/mss_gpio/mss_gpio.h \
+ ../firmware/drivers/mss_gpio/../../CMSIS/m2sxxx.h \
+ C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cm3.h \
+ C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cmInstr.h \
+ C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/cmsis_gcc.h \
+ C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cmFunc.h \
+ ../firmware/CMSIS/system_m2sxxx.h
+
+../firmware/drivers/mss_gpio/mss_gpio.h:
+
+../firmware/drivers/mss_gpio/../../CMSIS/m2sxxx.h:
+
+C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cm3.h:
+
+C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cmInstr.h:
+
+C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/cmsis_gcc.h:
+
+C:/Microsemi/SoftConsole_v5.1/CMSIS/V4.5/Include/core_cmFunc.h:
+
+../firmware/CMSIS/system_m2sxxx.h:
